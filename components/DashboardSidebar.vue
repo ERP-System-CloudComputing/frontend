ec2d52cb84bde69b972254ffc1494ffeb5cfdfbb
Mario Cobian <marvanis@hotmail.com>
2025-05-12 20:25:00 -0600

    [ADDED]: Comienzo para realizar el sidebar y topbar de todas las paginas

---
+++ components/DashboardSidebar.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-navigation-drawer
+    app
+    permanent
+  >
+    <div>
+      <v-img
+        src="/logo.svg"
+        class="w-full"
+      />
+      <div>
+        <h4>UIUxOtor</h4>
+        <span>ERP System</span>
+      </div>
+    </div>
+  </v-navigation-drawer>
+</template>
+
+<script>
+
+// const menuItems = [
+//   { title: 'Dashboard', icon: 'mdi-view-dashboard', path: '/dashboard' },
+//   { title: 'Staff', icon: 'mdi-account-group', path: '/dashboard/staff' },
+//   { title: 'Payment Voucher', icon: 'mdi-cash', path: '/dashboard/vouchers' },
+//   { title: 'Payroll', icon: 'mdi-currency-usd', path: '/dashboard/payroll' },
+//   { title: 'Memo', icon: 'mdi-file-document-outline', path: '/dashboard/memos' },
+//   { title: 'Circulars', icon: 'mdi-email', path: '/dashboard/circulars' },
+//   { title: 'Maintenance', icon: 'mdi-wrench', path: '/dashboard/maintenance' },
+//   { title: 'Logistics', icon: 'mdi-truck-delivery', path: '/dashboard/logistics' },
+//   { title: 'Office Budget', icon: 'mdi-bank', path: '/dashboard/budget' },
+//   { title: 'Stocks and Inventory', icon: 'mdi-warehouse', path: '/dashboard/stocks' },
+//   { title: 'Notifications', icon: 'mdi-bell', path: '/dashboard/notifications' },
+//   { title: 'Capacity Building', icon: 'mdi-school-outline', path: '/dashboard/capacity' },
+//   { title: 'Procurements', icon: 'mdi-cart-outline', path: '/dashboard/procurements' }
+// ]
+
+export default {
+
+}
+</script>
---
+++ layouts/principal.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <DashboardSidebarVue />
+  </v-app>
+</template>
+
+<script>
+import DashboardSidebarVue from '~/components/DashboardSidebar.vue'
+
+export default {
+  components: {
+    DashboardSidebarVue
+  }
+}
+</script>

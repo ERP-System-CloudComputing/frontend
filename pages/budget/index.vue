29027245a570bd27188eab42f3c40da36ae04ee5
Mario Cobian <marvanis@hotmail.com>
2025-05-17 20:45:05 -0600

    [ADDED]: Guardado de los cambios

--- pages/budget/index.vue
+++ pages/budget/index.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
+    <BudgetCardsVue />
+
+    <div>
+      <h3>Create a Budget</h3>
+
+      <v-spacer />
+
+      <v-button></v-button>
+    </div>
   </div>
 </template>
 
 <script>
+import BudgetCardsVue from '~/components/budget/BudgetCards.vue'
+
 export default {
+  components: {
+    BudgetCardsVue
+  },
   layout: 'principal'
 }
 </script>

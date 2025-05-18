0c89df06f33af74db50ffb12c48c8df011f62133
Mario Cobian <marvanis@hotmail.com>
2025-05-17 22:24:28 -0600

    [ADDED]: Vista principal de Budget realizado

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
       colors: {
         "azul-claro-logo": "#14ADD6",
         "azul-oscuro-logo": "#384295",
-        "gray-fondo": "#F8F9FD"
+        "gray-fondo": "#F8F9FD",
+        primario: '#14ADD6',
+        secundario: '#384295',
+        gris_claro: '#4b5563',
+        negro: '#000000'
       }
     },
   },

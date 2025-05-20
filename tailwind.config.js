5822d5ab58ff7839a6604ca88ffa02aa88b0fab4
Mario Cobian <marvanis@hotmail.com>
2025-05-20 09:30:20 -0600

    [ADDED]: Configuracion del tailwind despues del merge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,40 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts}'],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts}',
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
+        primario: '#14ADD6',
+        secundario: '#384295',
+        gris_claro: '#4b5563',
+        negro: '#000000',
         'azul-claro-logo': '#14ADD6',
         'azul-oscuro-logo': '#384295',
-        'gray-fondo': '#F8F9FD',
-        darkMode: false, // or 'media' or 'class'
-        content: [
-          './components/**/*.{vue,js,ts}',
-          './layouts/**/*.vue',
-          './pages/**/*.vue',
-          './plugins/**/*.{js,ts}',
-          './nuxt.config.{js,ts}'
-        ],
-        theme: {
-          extend: {
-            colors: {
-              primario: '#14ADD6',
-              secundario: '#384295',
-              gris_claro: '#4b5563',
-              negro: '#000000'
-            },
-            backgroundImage: {
-              'gradient-primario-secundario': 'linear-gradient(to right, #14ADD6, #384295)'
-            },
-            width: {
-              '25rem': '25rem'
-            }
-          }
-        },
-        variants: {
-          extend: {}
-        },
-        plugins: [
-          require('@tailwindcss/forms'),
-          function ({ addVariant }) {
-            addVariant('before', '&::before')
-          }
-        ]
+        'gray-fondo': '#F8F9FD'
+      },
+      backgroundImage: {
+        'gradient-primario-secundario': 'linear-gradient(to right, #14ADD6, #384295)'
+      },
+      width: {
+        '100': '25rem' // Tailwind recomienda usar claves numéricas o semánticas
       }
     }
-  }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    function ({ addVariant }) {
+      addVariant('before', '&::before')
+    }
+  ]
 }

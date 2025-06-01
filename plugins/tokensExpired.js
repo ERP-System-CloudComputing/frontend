0fec52363bf508500378c216a5f4bdfc9ae88772
Luis-Alvarezz <luisangelug10@gmail.com>
2025-05-31 18:06:09 -0600

    [MODIFY]: Se mejoro logica para el manejo de actualizacion de token por inactividad

--- plugins/tokensExpired.js
+++ plugins/tokensExpired.js
@@ -2,6 +2,7 @@ export default function ({ $axios, $auth, redirect }) {
   $axios.onError(async (error) => {
     const originalRequest = error.config
 
+    // * Si el error es de tipo 401 y no se ha intentado refrezcar el token:
     if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true
 
@@ -18,8 +19,8 @@ export default function ({ $axios, $auth, redirect }) {
         originalRequest.headers.Autorization = `Bearer ${accessToken}`
         return $axios(originalRequest)
       } catch (refreshError) {
-        $auth.logout()
-        redirect('/')
+        await $auth.logout()
+        redirect('/?sesion=expired&reason=inactivity')
         return Promise.reject(refreshError)
       }
     }

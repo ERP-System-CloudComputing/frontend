c8f26dfbf07e6e95692abf1c185c20ac89bc28e5
Mario Cobian <marvanis@hotmail.com>
2025-05-31 23:36:14 -0600

    [MODIFY]: Campos del formulario de budget actualizados: Muestran errores

--- pages/budget/create.vue
+++ pages/budget/create.vue
@@ -14,43 +14,62 @@
       <v-card-subtitle>Kindly fill in the form below to create a budget</v-card-subtitle>
 
       <v-card-text>
-        <v-row dense>
-          <v-col cols="12" md="4">
-            <label class="text-sm font-normal text-black">Budget number</label>
-            <v-text-field v-model="budget.budgetNo" placeholder="Enter item" outlined dense />
-          </v-col>
-          <v-col cols="12" md="4">
-            <label class="text-sm font-normal text-black">Budget description</label>
-            <v-text-field v-model="budget.description" placeholder="Enter description" outlined dense />
-          </v-col>
-          <v-col cols="12" md="4">
-            <label class="text-sm font-normal text-black">Budget amount</label>
-            <v-text-field v-model="budget.amount" placeholder="Enter amount in $" outlined dense prefix="$" />
-          </v-col>
+        <v-form ref="form" v-model="isFormValid">
+          <v-row dense>
+            <v-col cols="12" md="4">
+              <label class="text-sm font-normal text-black">Budget number</label>
+              <v-text-field v-model="budget.number" placeholder="Enter item" outlined dense :rules="[rules.required, rules.positiveAmount]" />
+            </v-col>
+            <v-col cols="12" md="4">
+              <label class="text-sm font-normal text-black">Budget description</label>
+              <v-text-field v-model="budget.description" placeholder="Enter description" outlined dense :rules="[rules.required]" />
+            </v-col>
+            <v-col cols="12" md="4">
+              <label class="text-sm font-normal text-black">Budget amount</label>
+              <v-text-field
+                v-model="budget.amount"
+                placeholder="Enter amount in $"
+                outlined
+                dense
+                prefix="$"
+                :rules="[rules.required, rules.positiveAmount]"
+                @blur="formatAmount"
+              />
+            </v-col>
 
-          <v-col cols="12" md="4">
-            <label class="text-sm font-normal text-black">Date</label>
-            <v-text-field v-model="budget.date" placeholder="DD/MM/YYYY" outlined dense type="date" />
-          </v-col>
-          <v-col cols="12" md="4">
-            <label class="text-sm font-normal text-black">Receiving office</label>
-            <v-select
-              v-model="budget.office"
-              :items="offices"
-              placeholder="Select office"
-              outlined
-              dense
-            />
-          </v-col>
-        </v-row>
+            <v-col cols="12" md="4">
+              <label class="text-sm font-normal text-black">Date</label>
+              <v-text-field
+                v-model="budget.requestDate"
+                placeholder="DD/MM/YYYY"
+                outlined
+                dense
+                type="date"
+                :rules="[rules.required]"
+              />
+            </v-col>
+            <v-col cols="12" md="4">
+              <label class="text-sm font-normal text-black">Receiving office</label>
+              <v-select
+                v-model="budget.receivingOffice"
+                :items="offices"
+                placeholder="Select office"
+                outlined
+                dense
+                :rules="[rules.required]"
+              />
+            </v-col>
+          </v-row>
 
-        <v-btn
-          color="primary"
-          class="w-full max-w-xs px-8 py-3 bg-gradient-to-r from-primario to-secundario text-white rounded-lg
-              hover:opacity-90 duration-500 transform hover:scale-105 ease-in-out color-white"
-        >
-          Create Budget
-        </v-btn>
+          <v-btn
+            color="primary"
+            class="w-full max-w-xs px-8 py-3 bg-gradient-to-r from-primario to-secundario text-white rounded-lg
+                hover:opacity-90 duration-500 transform hover:scale-105 ease-in-out color-white"
+            @click="createBudget"
+          >
+            Create Budget
+          </v-btn>
+        </v-form>
       </v-card-text>
     </v-card>
 
@@ -88,12 +107,13 @@ export default {
   layout: 'principal',
   data () {
     return {
+      isFormValid: false,
       budget: {
-        budgetNo: '',
+        number: '',
         description: '',
         amount: '',
-        date: '',
-        office: ''
+        requestDate: '',
+        receivingOffice: ''
       },
       offices: ['Head Office', 'Regional Office', 'Branch Office'],
       budgetList: [],
@@ -103,7 +123,31 @@ export default {
         { text: 'Budget Description', value: 'description' },
         { text: 'Budget Amount (MXN)', value: 'amount' },
         { text: 'Date', value: 'date' }
-      ]
+      ],
+      rules: { // Define tus reglas de validación
+        required: value => !!value || 'Este campo es obligatorio.',
+        positiveAmount: value => (value && parseFloat(value) > 0) || 'Debe contener números positivos.'
+      }
+    }
+  },
+  methods: {
+    formatAmount () {
+      const value = this.budget.amount.toString().replace(/,/g, '') // quitar comas si las hay
+      const number = parseFloat(value)
+
+      if (!isNaN(number)) {
+        this.budget.amount = number.toFixed(2) // guarda con dos decimales
+      } else {
+        this.budget.amount = ''
+      }
+    },
+    createBudget () {
+      if (!this.$refs.form.validate()) {
+        console.warn('Formulario inválido')
+        return
+      }
+
+      console.log(this.budget)
     }
   }
 }

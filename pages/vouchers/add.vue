71215c2ce4681101f35d973b36cf65ed37749596
AntonioC003 <ma.castilloquintanilla@ugto.mx>
2025-06-03 01:54:24 -0600

    [ADDED] Calculando montos de vouchers y ajustando v-data-table

--- pages/vouchers/add.vue
+++ pages/vouchers/add.vue
@@ -19,53 +19,51 @@
       </v-col>
       <v-col cols="12">
         <v-data-table
-          class="w-full rounded-xl"
+          class="w-full rounded-xl max-h-40 overflow-y-auto overflow-x-hidden"
           :headers="headers"
-          :items="vouchers"
-          :items-per-page="itemsPerPage"
+          :items="formBuyDataList"
           :page.sync="page"
           hide-default-footer
           dense
         >
-          <template #footer>
-            <v-row class=" border-b-2">
-              <v-col cols="12" md="2">
-                <div class="border border-gray-300 rounded-xl p-4 hover:bg-gray-100">
-                  <button @click="openDialog()">
-                    <v-icon>
-                      mdi-plus
-                    </v-icon>
-                    <span class="sm:pl-5"> Add another row</span>
-                  </button>
-                </div>
-              </v-col>
-              <v-col cols="12">
-                <v-row>
-                  <v-col cols="12" sm="3">
-                    <h1 class="font-bold text-lg">
-                      Total
-                    </h1>
-                  </v-col>
-                  <v-col cols="12" sm="3">
-                    <span class="font-bold text-lg">
-                      wait
-                    </span>
-                  </v-col>
-                  <v-col cols="12" sm="3">
-                    <span class="font-bold text-lg">
-                      wait
-                    </span>
-                  </v-col>
-                  <v-col cols="12" sm="3">
-                    <span class="font-bold text-lg">
-                      wait
-                    </span>
-                  </v-col>
-                </v-row>
-              </v-col>
-            </v-row>
+          <template #[`item.sn`]="{ index }">
+            {{ index + 1 }}
           </template>
         </v-data-table>
+      </v-col>
+      <v-col cols="12" md="2">
+        <div class="border border-gray-300 rounded-xl p-4 hover:bg-gray-100">
+          <button @click="openDialog()">
+            <v-icon>
+              mdi-plus
+            </v-icon>
+            <span class="sm:pl-5"> Add another row</span>
+          </button>
+        </div>
+      </v-col>
+      <v-col cols="12">
+        <v-row>
+          <v-col cols="12" sm="3">
+            <h1 class="font-bold text-lg">
+              Total
+            </h1>
+          </v-col>
+          <v-col cols="12" sm="3">
+            <span class="font-bold text-lg">
+              wait
+            </span>
+          </v-col>
+          <v-col cols="12" sm="3">
+            <span class="font-bold text-lg">
+              wait
+            </span>
+          </v-col>
+          <v-col cols="12" sm="3">
+            <span class="font-bold text-lg">
+              wait
+            </span>
+          </v-col>
+        </v-row>
       </v-col>
       <v-col cols="12">
         <span>
@@ -134,29 +132,32 @@
           Create Payment Voucher
         </v-card-title>
         <v-card-text>
-          <v-form>
+          <v-form ref="form">
             <v-row>
-              <v-col class="12" sm="4">
+              <v-col cols="12" sm="4">
                 <span>Class</span>
                 <v-text-field
+                  v-model="formBuyData.class"
                   class="rounded-lg"
                   placeholder="Enter class"
                   dense
                   outlined
                 />
               </v-col>
-              <v-col class="12" sm="4">
+              <v-col cols="12" sm="4">
                 <span>Description</span>
                 <v-text-field
+                  v-model="formBuyData.description"
                   class="rounded-lg"
                   placeholder="Enter description"
                   dense
                   outlined
                 />
               </v-col>
-              <v-col class="12" sm="4">
+              <v-col cols="12" sm="4">
                 <span>QTY</span>
                 <v-text-field
+                  v-model="formBuyData.qty"
                   class="rounded-lg"
                   placeholder="Enter QTY"
                   dense
@@ -168,6 +169,7 @@
               <v-col class="12" sm="4">
                 <span class="font-bold">Unit Price (N)</span>
                 <v-text-field
+                  v-model="formBuyData.unitPrice"
                   class="rounded-lg"
                   placeholder="Enter unit price"
                   dense
@@ -177,15 +179,17 @@
               <v-col class="12" sm="4">
                 <span class="font-bold">Amount (N)</span>
                 <v-text-field
+                  v-model="formBuyData.amount"
                   class="rounded-lg"
-                  placeholder="Enter description"
                   dense
                   outlined
+                  disabled
                 />
               </v-col>
               <v-col class="12" sm="4">
                 <span class="font-bold">VAT %</span>
                 <v-text-field
+                  v-model="formBuyData.vat"
                   class="rounded-lg"
                   placeholder="Enter QTY"
                   dense
@@ -194,25 +198,30 @@
               </v-col>
             </v-row>
             <v-row>
-              <v-col class="12" sm="4">
+              <v-col cols="12" sm="4">
                 <span class="font-bold">VAT Amount (N)</span>
                 <v-text-field
+                  v-model="formBuyData.vatAmount"
                   class="rounded-lg"
                   dense
                   outlined
+                  disabled
                 />
               </v-col>
-              <v-col class="12" sm="4">
+              <v-col cols="12" sm="4">
                 <span class="font-bold">Gross Amount (N)</span>
                 <v-text-field
+                  v-model="formBuyData.grossAmount"
                   class="rounded-lg"
                   dense
                   outlined
+                  disabled
                 />
               </v-col>
-              <v-col class="12" sm="4">
+              <v-col cols="12" sm="4">
                 <span class="font-bold">WHT %</span>
                 <v-text-field
+                  v-model="formBuyData.wht"
                   class="rounded-lg"
                   placeholder="Enter WHT"
                   dense
@@ -221,20 +230,24 @@
               </v-col>
             </v-row>
             <v-row>
-              <v-col class="12" sm="6">
+              <v-col cols="12" sm="6">
                 <span class="font-bold">WHT Amount (N)</span>
                 <v-text-field
+                  v-model="formBuyData.whtAmount"
                   class="rounded-lg"
                   dense
                   outlined
+                  disabled
                 />
               </v-col>
-              <v-col class="12" sm="6">
+              <v-col cols="12" sm="6">
                 <span class="font-bold">Net Amount (N)</span>
                 <v-text-field
+                  v-model="formBuyData.netAmount"
                   class="rounded-lg"
                   dense
                   outlined
+                  disabled
                 />
               </v-col>
             </v-row>
@@ -242,12 +255,12 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <div>
-            <button>
-              ADD
-            </button>
-            <button>
+          <div class="flex space-x-4">
+            <button class=" text-white w-full sm:mt-4 justify-center rounded-lg bg-gradient-to-br from-yellow-500 to-red-800 hover:from-red-900 shadow-md p-4" @click="closeDialog()">
               Cancel
+            </button>
+            <button class=" text-white w-full sm:mt-4 justify-center rounded-lg bg-gradient-to-br from-primario to-secundario hover:from-blue-600 shadow-md p-4" @click="createBuy()">
+              Create
             </button>
           </div>
         </v-card-actions>
@@ -275,13 +288,72 @@ export default {
         { text: 'WHT Amount', value: 'whtAmount' },
         { text: 'Net Amount', value: 'netAmount' }
       ],
-      confirmDialog: false
+      confirmDialog: false,
+      voucherData: {
+        userId: '',
+        subject: '',
+        date: '',
+        preparedBy: '',
+        sendTo: ''
+      },
+      formBuyDataList: [],
+      formBuyData:
+        {
+          class: '',
+          description: '',
+          qty: '',
+          unitPrice: '',
+          amount: '',
+          vat: '',
+          vatAmount: '',
+          grossAmount: '',
+          wht: '',
+          whtAmount: '',
+          netAmount: ''
+        }
+    }
+  },
+  watch: {
+    formBuyData: { // - Calculando costos
+      handler () {
+        this.formBuyData.amount = parseFloat(this.formBuyData.unitPrice) || ''
+        this.formBuyData.vatAmount = parseFloat(this.formBuyData.amount) * (parseFloat(this.formBuyData.vat) / 100).toFixed(2) || ''
+        this.formBuyData.grossAmount = parseFloat(this.formBuyData.amount) + parseFloat(this.formBuyData.vatAmount) || ''
+        this.formBuyData.whtAmount = parseFloat(this.formBuyData.amount) * (parseFloat(this.formBuyData.wht) / 100).toFixed(2) || ''
+        this.formBuyData.netAmount = parseFloat(this.formBuyData.grossAmount) - parseFloat(this.formBuyData.whtAmount).toFixed(2) || ''
+      },
+      deep: true
     }
   },
   methods: {
+    returnAll () {
+      this.$router.push('/vouchers')
+    },
     openDialog () {
       this.confirmDialog = true
+      this.formBuyData = {
+        class: '',
+        description: '',
+        qty: '',
+        unitPrice: '',
+        amount: '',
+        vat: '',
+        vatAmount: '',
+        grossAmount: '',
+        wht: '',
+        whtAmount: '',
+        netAmount: ''
+      }
+    },
+    closeDialog () {
+      this.confirmDialog = false
+    },
+    createBuy () {
+      this.formBuyDataList.push({ ...this.formBuyData })
+      //  console.log(this.formBuyDataList)
+      this.confirmDialog = false
     }
+
   }
 
 }

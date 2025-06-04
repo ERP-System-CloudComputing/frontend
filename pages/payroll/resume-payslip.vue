651f76dac2ba1dd2a6132a6ac14aaa6c0284a2b6
Luis-Alvarezz <luisangelug10@gmail.com>
2025-06-04 16:18:03 -0600

    [ADDED]: RUTA para subComponente ResumePayslip

---
+++ pages/payroll/resume-payslip.vue
@@ -0,0 +1,14 @@
+<template>
+  <ResumePayslip />
+</template>
+
+<script>
+import ResumePayslip from '~/components/payroll/subComponets/payrollResumePayslip.vue'
+export default {
+  name: 'ResumePayslip',
+  components: {
+    ResumePayslip
+  },
+  layout: 'principal'
+}
+</script>

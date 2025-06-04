351fba7f4aaa9f2367f41ae0986bf58f4fff1103
Luis-Alvarezz <luisangelug10@gmail.com>
2025-06-04 01:30:31 -0600

    [ADDED]: Nuevo archivo para ejecutar RUTA de sub-componente

---
+++ pages/payroll/create-TaxDefinition.vue
@@ -0,0 +1,13 @@
+<template>
+  <PayrollCreateTax />
+</template>
+
+<script>
+import PayrollCreateTax from '~/components/payroll/subComponets/payrollCreateTax.vue'
+export default {
+  components: {
+    PayrollCreateTax
+  },
+  layout: 'principal'
+}
+</script>

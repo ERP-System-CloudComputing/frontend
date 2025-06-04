a42ccdd9cb5638cfd4e52a04ac50376baa687318
Luis-Alvarezz <luisangelug10@gmail.com>
2025-06-04 16:17:32 -0600

    [ADDED]: RUTA para subComponente CreatePayslip

---
+++ pages/payroll/create-payslip.vue
@@ -0,0 +1,13 @@
+<template>
+  <PayrollCreatePayslip />
+</template>
+
+<script>
+import PayrollCreatePayslip from '~/components/payroll/subComponets/payrollCreatePayslip.vue'
+export default {
+  components: {
+    PayrollCreatePayslip
+  },
+  layout: 'principal'
+}
+</script>

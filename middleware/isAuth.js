24479d10376abfc0fb7d6566012abb1125f7dc5d
Luis-Alvarezz <luisangelug10@gmail.com>
2025-06-05 23:08:25 -0600

    [MODIFY]: Se añadio implementacion unicamente del lado del cliente para cuando se activa rememberMe + Nuxt

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -3,7 +3,6 @@ export default function ({ store, redirect, route, app }) {
   if (process.client) {
     // * Si el usuario esta autenticado o Nuxt Auth lo rehidrato y esta intentanco acceeder a la pagina de login:
     if (app.$auth.loggedIn && route.path === '/') {
-      console.log('Oh, dejaste el rememebeME activo, se re redigira a: ', route.path)
       return redirect('/dashboard')
     }
   }

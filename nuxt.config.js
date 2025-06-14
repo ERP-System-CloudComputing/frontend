66eec42354a3de32a4067a7890a57104e7f72adf
Luis-Alvarezz <luisangelug10@gmail.com>
2025-06-13 20:47:03 -0600

    [MODIFY]: URL en la Nube y local

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,7 +58,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // baseURL: process.env.APP_URL
-    baseURL: process.env.APP_URL || 'http://localhost:5050/api/',
+    baseURL: process.env.APP_URL || 'https://backend-erp-system-cxenaxhufchmf6d2.canadacentral-01.azurewebsites.net/api',
     credentials: true, // * Para las cookies,
     headers: {
       common: {
